6310b0124ebc3db8b708b212a27db2a0580aaee2
Suvodeep Das <suvodeep568@gmail.com>
2023-03-20 13:10:58 +0530

    Add files via upload

---
+++ OOPS/multiply two complex values/mult.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class mult{
+public static class complexnumber
+{
+ double real;
+  double imaginary;
+  complexnumber(){}
+  complexnumber(double real,double imaginary)
+{
+  this.real=real;
+  this.imaginary=imaginary;
+}
+}
+
+public static complexnumber mul(complexnumber x, complexnumber y)
+{
+  complexnumber obj= new complexnumber();
+  obj.real=x.real*y.real;
+  obj.imaginary=x.imaginary*y.imaginary;
+  return obj; 
+}
+
+ public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  complexnumber num1=new complexnumber();
+  complexnumber num2=new complexnumber();
+  System.out.println("Enter the first real part of complex number: ");
+  num1.real=sc.nextDouble();
+  System.out.println("Enter the first imaginary part of complex number: ");
+  num1.imaginary=sc.nextDouble();
+  System.out.println("Enter the second real part of complex number: ");
+  num2.real=sc.nextDouble();
+  System.out.println("Enter the second imaginary part of complex number: ");
+  num2.imaginary=sc.nextDouble();
+  complexnumber ans= mul(num1,num2);
+  System.out.println("The multiplication of two complex number is: "+ans.real+"+"+ans.imaginary+"i"); 
+}
+}
