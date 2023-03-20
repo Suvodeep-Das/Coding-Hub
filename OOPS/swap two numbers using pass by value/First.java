7e0636cecb0c5769a9c5aa4d03da015bda2a83bf
Suvodeep Das <suvodeep568@gmail.com>
2023-03-20 13:07:56 +0530

    Add files via upload

---
+++ OOPS/swap two numbers using pass by value/First.java
@@ -0,0 +1,30 @@
+class Student
+{
+    String name,stream,college;
+    char grade;
+    int roll;
+    public Student(String n,String s,String c,char g,int r)
+    {
+        name=n;
+        stream=s;
+        college=c;
+        grade=g;
+        roll=r;
+    }
+    public void display()
+    {
+     System.out.println("NAME : "+this.name); 
+     System.out.println("ROLL_NO : "+this.roll); 
+     System.out.println("STREAM : "+this.stream); 
+     System.out.println("COLLEGE : "+this.college); 
+     System.out.println("GRADE : "+this.grade);
+    }
+    public static void main(String args[])
+    {
+       Student s1=new Student("Suvodeep Das","CSE","IEM",'B',90);
+       Student s2=new Student("Amlan Das","IT","IEM",'b',95); 
+       s1.display();
+       s2.display(); 
+        
+    }
+}
